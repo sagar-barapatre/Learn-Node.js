27fab0b1e571e0440592aa4285bcca8cd4fdd93f
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-09-09 08:38:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// const productRouter = require('./router/product');
+// const shopRouter = require('./router/shop');
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// app.use(productRouter);
+// app.use(shopRouter);
+
+app.use('/add-product', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'views', 'product.html'));
+});
+
+app.use('/product', (req, res) => {
+    res.redirect('/')
+});
+
+app.use('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'shop.html'));
+});
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
